bb7d487969ffefe2322d8eca57c8ee77eb5e11a7
Łukasz Kostrzewa <lukko98@gmail.com>
2019-12-27 11:01:35 +0100

    Refactor + Links added

--- storm/src/containers/Test.js
+++ storm/src/containers/Test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Test.css';
+import {Link} from "react-router-dom";
 
 export default function Test(props)
 {
@@ -36,6 +37,7 @@ export default function Test(props)
                             </tr>
                         )
                     })}
+                    <p><Link to={"/admin"}>Powrot</Link></p>
                 </tbody>
             
         </table>
--- storm/src/containers/Test/CreateTest.js
+++ storm/src/containers/Test/CreateTest.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {Link} from "react-router-dom";
 
 class Form extends React.Component {
     state = {
@@ -108,6 +109,7 @@ class Form extends React.Component {
                     })
                 }
                 <button onClick={this.addToBase}>Dodaj</button>
+                <p><Link to={"/admin"}>Powrot</Link></p>
             </form>
 
 
