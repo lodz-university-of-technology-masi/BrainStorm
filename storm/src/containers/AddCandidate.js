45a065a2bd4dd0dd72499a011fe0f67795086020
Szymon Dobrowolski <216747@edu.p.lodz.pl>
2020-01-06 12:53:56 +0100

    dodawanie,usuwanie,wyswietlanie kandydatow

---
+++ storm/src/containers/AddCandidate.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
+
+
+class AddCandidate extends React.Component {
+    constructor(props) {
+        super(props);
+        this.routeChange = this.routeChange.bind(this);
+        this.state = {userName:"",password:""};
+    }
+
+    routeChange(){
+        var user = JSON.stringify(this.state)
+        var xmlHttp = new XMLHttpRequest()
+        xmlHttp.open("POST", "https://f628s6t6a9.execute-api.us-east-1.amazonaws.com/ss/candidates/add", false)	
+        xmlHttp.setRequestHeader("Accept", "application/json")
+        // console.log(user)
+        xmlHttp.send(user)
+
+        let path = `/candidates` ;
+        this.props.history.push({
+            pathname: path,
+            
+        });
+    }
+
+    handleChangeName = (e) => {
+        this.state.userName = e.target.value
+    }
+
+    handleChange = (e) => {
+        if(e.target.type == "text"){
+            this.state.userName = e.target.value
+        }
+        else{
+            this.state.password = e.target.value
+        }
+    }
+
+    handleChangePassword = (e) => {
+        this.state.password = e.target.value
+    }
+
+    render(){
+        return(
+            <div>
+             <form onChange={this.handleChange}>
+             <input type="text" defaultValue={this.state.value}/>
+             <input type="password" defaultValue={this.state.value} minLength="6"/>
+             <button onClick={this.routeChange}>Dodaj</button>
+             </form>
+             
+            </div>
+            )
+        }
+        }
+        
+        export default AddCandidate;
