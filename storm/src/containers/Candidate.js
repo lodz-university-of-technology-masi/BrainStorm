d6fbdad5e577b67c181a06ed48ebaafa2e683c08
Szymon Gruda <szymon_gruda@o2.pl>
2020-01-13 14:41:14 +0100

    tests from dynamodb

--- storm/src/containers/Candidate.js
+++ storm/src/containers/Candidate.js
@@ -1,70 +1,65 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import {Auth} from "aws-amplify";
-
-let USERNAME = "start";
-export default function Candidate(props){
-
-
-    var xmlHttp = new XMLHttpRequest();
-    
-    Auth.currentAuthenticatedUser().then(user => {
-        USERNAME = user.username;
-        console.log("ustawiony username: " + USERNAME);
-    }).catch(e => console.log(e));
-    // xmlHttp.open("GET", "https://f628s6t6a9.execute-api.us-east-1.amazonaws.com/ss/candidate/" + "rutek", false);
-    xmlHttp.open("GET", "https://f628s6t6a9.execute-api.us-east-1.amazonaws.com/ss/candidate/" + USERNAME, false);	
-    xmlHttp.setRequestHeader("Accept", "application/json");
-    xmlHttp.send(null);
-    const dane = JSON.parse(xmlHttp.response);
-    // const ifer =[""];
-return(
-    <div>   
-        <h1>Dostępne testy </h1>
-                <table class="table" style={{backgroundColor: "lightgray"}}>
-                    <thead>
-                    <tr>
-                        <th>Id testu</th>
-                        <th>Rodzaj testu</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    { dane.map((test) => {
-                        if(test.points === "-1"){
-							return (
-								<tr>
-									<td>{ test.id }</td>
-									<td>{ test.title }</td>
-                                    <td><Link to={ "/candidate/solve/" + test.id }>Rozwiaz</Link></td>
-								</tr>
-                            )
-                        }
-                        else if(test.points !== "-2"){
-                            return (
-								<tr>
-									<td>{ test.id }</td>
-									<td>{ test.title }</td>
-                                    <td><Link to={ "/candidate/rate/" + test.id }>Sprawdź wynik</Link></td>
-								</tr>
-                            )
-                        }
-						}) }
-                    </tbody>
-
-                </table>
-                 <button onClick={becomeRecruiter}>Sprawdź swój status</button> 
-
-    </div>
-)
-
-async function becomeRecruiter(event) {
-    event.preventDefault();
-    try {
-        let user = await Auth.currentAuthenticatedUser();
-        props.userHasAuthenticated(true);
-        const result = await Auth.updateUserAttributes(user, {'custom:isRecruiter': '1'});
-    }catch(e){
-        alert(e)
+import {Button} from "react-bootstrap";
+  
+class CandidateTests extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            assignedTests: JSON.parse('{"tests":[]}'),
+            username: "",
+            ready: false,
+            dane: "",
+        }
+        Auth.currentAuthenticatedUser().then(user => {
+                console.log(user.username);
+                this.setState({username: user.username});
+                this.getAssignmentTable();
+                console.log(this.state.dane);
+            });
     }
+    
+
+    render(){
+    return(
+        <div>   
+            <h1>Dostępne testy </h1>
+                    <table class="table" style={{backgroundColor: "lightgray"}}>
+                        <thead>
+                        <tr>
+                            <th>Id testu</th>
+                            <th>Rodzaj testu</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                        Object.keys(this.state.dane).map((key) => {
+                            return(
+                                <tr>
+                                </tr>
+                            );
+                        })
+                     }
+                   
+                        </tbody>
+    
+                    </table>
+    
+        </div>
+    )}
+    
+    getAssignmentTable = () => {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", "https://f628s6t6a9.execute-api.us-east-1.amazonaws.com/ss/candidate/" + this.state.username, false);
+        // xmlHttp.open("GET", "https://f628s6t6a9.execute-api.us-east-1.amazonaws.com/ss/candidate/" + USERNAME, false);	
+        xmlHttp.setRequestHeader("Accept", "application/json");
+        xmlHttp.send(null);
+        this.state.dane = JSON.parse(xmlHttp.response);
+        // this.state.dane.map((test));
+        // console.log(this.state.dane[Object.keys(this.state.dane)[0]]);
+        };
 }
-}
+
+
+export default CandidateTests;
