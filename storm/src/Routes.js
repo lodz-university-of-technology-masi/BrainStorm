e822079c1376aa670bfaee26a0c5ccf94810a0db
Szymon <s.rutkowski235@gmail.com>
2020-01-15 13:54:22 +0100

    Routing bug fix

--- storm/src/Routes.js
+++ storm/src/Routes.js
@@ -29,7 +29,8 @@ export default function Routes({ appProps }) {
       <AuthorizedCandidateRoute path="/candidate/solve/:id" exact component={SolveTest} appProps={appProps}/>
       <AuthorizedCandidateRoute path="/candidate/rate/:id" exact component={Rate} appProps={appProps}/>
       <AuthorizedRecruiterRoute path="/admin/candidates/add" exact component={AddCandidate} appProps={appProps} />
-      <AuthorizedRecruiterRoute Route exact path="/admin/createTest" component={(props) => <CreateTest {...props}/>} />
+      {/*<AuthorizedRecruiterRoute Route exact path="/admin/createTest" component={(props) => <CreateTest {...props}/>} />*/}
+      <AuthorizedRecruiterRoute path="/admin/createTest" exact component={CreateTest} appProps={appProps} />
      <Route component={NotFound} /> { /* Finally, catch all unmatched routes */ }
     </Switch>
   );
